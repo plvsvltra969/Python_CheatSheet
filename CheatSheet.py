6bc8138e045c78bea489adef09ff24ed2b03595b
plvsvltra969 <34136128+plvsvltra969@users.noreply.github.com>
2017-12-01 23:32:28 -0500

    Update CheatSheet.py

--- CheatSheet.py
+++ CheatSheet.py
@@ -1,4 +1,4 @@
-# Hello! This is my Python Cheat Sheet
+# Hello! This is my Python Cheat Sheet! This is all original content
 # Firstly, "#" Is used to create single-line comments in Python
 
 """ Multi-line comments are done by using 3 quotation marks in a row
@@ -15,9 +15,40 @@ print  "5+5 = " + str(example_one)
 example_two = 100-50 
 print "100-50 = " + str(example_two)
 
-#To be continued...
+#Multiplication
+example_three = 5*5 
+print "5*5 = " + str(example_three)
+
+#Division 
+example_four = 81/9
+print "81/9 = " + str(example_four)
+
+#Exponential functions 
+example_five = 4**4
+print "4**2 =" +str(example_five) 
+
+#Modulus: The Modulus operator is used for remainder division on integers
+example_six = 27%5 
+print "27%5 = " + str(example_six)
+
+#Data Types: 
+#------------------------------------------------------------------
+
+#Varibles - Store a value for later use
+
+#Data types can be strings, which contain letters, numbers, and sybmbols
+varible_one = "String"
+print "varible_one = " + varible_one
+#Data types can be numbers
+varible_two = 50
+print "varible_two = " + str(varible_two)
+#Data types can be boolean values (True/False)
+varible_three = True
+print "varible_three = " + str(varible_three)
+
+#Strings behave in such a manner where each character is assigned a number, called the index.
+""" [String__varible = string] S | T | R | I | N | G |
+                               0 | 1 | 2 | 3 | 4 | 5 |  """
 
 
-
-
-
+#STILL WORK IN PROGRESS
