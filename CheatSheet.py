d70c65ae0f9d0dcdb7a6677d9418e5dc449afff7
plvsvltra969 <34136128+plvsvltra969@users.noreply.github.com>
2017-12-01 22:49:41 -0500

    Create CheatSheet.py

---
+++ CheatSheet.py
@@ -0,0 +1,23 @@
+# Hello! This is my Python Cheat Sheet
+# Firstly, "#" Is used to create single-line comments in Python
+
+""" Multi-line comments are done by using 3 quotation marks in a row
+this makes commenting easier in paragraph form rather than using a hundred Hashtag comment lines
+the triple quotation mark is helpful in this context """
+
+#Python is capable of basic arithmetic 
+
+#Addition 
+example_one = 5+5 
+print  "5+5 = " + str(example_one)
+
+#Subtraction 
+example_two = 100-50 
+print "100-50 = " + str(example_two)
+
+#To be continued...
+
+
+
+
+
